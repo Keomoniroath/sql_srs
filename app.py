e37e99e5ea7d3002af6ce0c4a35037b76ff8dd79
keomoniroath <keomoniroath.tao@gmail.com>
2024-05-21 10:11:01 +0200

    add theme selection

--- app.py
+++ app.py
@@ -2,7 +2,16 @@ import pandas as pd
 import streamlit as st
 import duckdb
 
-st.write('Hello world')
+st.write('SQL SRS')
+
+option = st.selectbox(
+    "What would you like to review ?",
+    ('Join', 'GroupBy', 'Windows functions'),
+    index=None,
+    placeholder='select a theme'
+)
+st.write('You selected:', option)
+
 data = {"a": [1, 2, 3], "b": [4, 5, 6]}
 df = pd.DataFrame(data)
 
