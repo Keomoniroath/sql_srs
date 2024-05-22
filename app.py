4a0942266e4c69f9bb8e642a443b8e056b6a91c8
keomoniroath <keomoniroath.tao@gmail.com>
2024-05-22 11:18:03 +0200

    comparaison with solution

--- app.py
+++ app.py
@@ -4,8 +4,6 @@ import streamlit as st
 
 con = duckdb.connect(database="data/exercises_sql_tables.duckdb", read_only=False)
 
-#solution = duckdb.sql(answer).df()
-
 with st.sidebar:
     theme = st.selectbox(
         "What would you like to review ?",
@@ -18,12 +16,29 @@ with st.sidebar:
     exercise = con.execute(f"SELECT * FROM memory_state WHERE theme = '{theme}'").df()
     st.write(exercise)
 
+    if not exercise.empty:
+        exercise_name = exercise.loc[0, "exercise_name"]
+        with open(f"answer/{exercise_name}.sql", "r") as f:
+            answer = f.read()
+
+    solution_df = con.execute(answer).df()
+
 st.header("enter your code:")
 query = st.text_area(label="votre code SQL ici", key="user_input")
 
 if query:
     result = con.execute(query).df()
     st.dataframe(result)
+
+    try:
+        result = result[solution_df.columns]
+        st.dataframe(result.compare(solution_df.columns))
+    except KeyError as e:
+        st.write('Le nombre de colonne est différent')
+
+    nb_row_diff = result.shape[0] - solution_df.shape[0]
+    if nb_row_diff != 0:
+        st.write(f' {nb_row_diff} lignes de différence')
 
 
 tab2, tab3 = st.tabs(["Tables", "Solution"])
@@ -36,8 +51,5 @@ with tab2:
         st.dataframe(df_table)
 
 with tab3:
-    exercise_name = exercise.loc[0, "exercise_name"]
-    with open(f"answer/{exercise_name}.sql", "r") as f:
-        answer = f.read()
     st.write(answer)
 
